ec1108d9cc775edbe0532df0dab0cf7c3dfadccb
Firestack <me@stackfire.io>
2015-08-23 02:18:54 -0600

    Moving on to other files in C#

---
+++ ADFixer/Core.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Security;
+using System.DirectoryServices.AccountManagement;
+using System.DirectoryServices.ActiveDirectory;
+
+namespace ADFixer
+{
+    public class ReadAD
+    {
+    
+        private string domainName;
+        private string User;
+        private string Password;
+        private PrincipalContext ctx;
+
+        public ReadAD()
+        {
+            Console.Write("Domain: ");
+            domainName = Console.ReadLine();
+
+            Console.Write("Username: ");
+            User = Console.ReadLine();
+
+            Console.Write("Password: ");
+            Password = Console.ReadLine();
+
+            ctx = new PrincipalContext(ContextType.Domain, domainName);
+            ctx.ValidateCredentials(User, Password);
+
+            Password = "";
+
+        }
+
+        public void ReadType<TYPE>() where TYPE : Principal
+        {
+            // Group Principal Searcher Creation
+            TYPE Access = new TYPE(ctx);
+
+            PrincipalSearcher search = new PrincipalSearcher(Access);
+
+            foreach (TYPE iter in search.FindAll())
+            {
+                Console.WriteLine("{0} - {1}", iter.SamAccountName, iter.Name);
+            }
+
+        }
+    }
+}
--- ADFixer/Program.cs
+++ ADFixer/Program.cs
@@ -1,9 +1,17 @@
 ﻿using System;
+using System.DirectoryServices.AccountManagement;
 using System.DirectoryServices.ActiveDirectory;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Security;
+
+
+
 
 namespace ADFixer
 {
@@ -11,15 +19,9 @@ namespace ADFixer
     {
         static void Main(string[] args)
         {
-            const long total = 10000;
-            
-            Console.WriteLine("Press go?");
-            Console.ReadKey();
 
-            ulong calc = 1;
-            for (uint i = (uint)total; i > 1; i--) { calc *= i; }
+            ADFixer.ReadAD NewAD = new ReadAD();
 
-            Console.Write("{0}\n", calc);
             //Exit Program
             Console.WriteLine("Press Any Key To Exit...");
             Console.ReadKey();
@@ -27,32 +29,106 @@ namespace ADFixer
             
         }
 
-        //static void DoStuff()
-        //{
-        //    string groupName = "Domain Users";
-        //    string domainName = "";
+        static void ReaadAD()
+        {
 
-        //    PrincipalContext ctx = new PrincipalContext(ContextType.Domain, domainName);
-        //    GroupPrincipal grp = GroupPrincipal.FindByIdentity(ctx, IdentityType.SamAccountName, groupName);
+            
 
-        //    if (grp != null)
-        //    {
-        //        foreach (Principal p in grp.GetMembers(false))
-        //        {
-        //            Console.WriteLine(p.SamAccountName + " - " + p.DisplayName);
-        //        }
+            string groupName = "Domain Admins";
+
+            Console.Write("Domain: ");
+            string domainName = Console.ReadLine();
+            Console.Write("Username: ");
+            string User = Console.ReadLine();
+            Console.Write("Password: ");
+            string Password = Console.ReadLine();
+
+            // Create Domain Context
+            PrincipalContext ctx = new PrincipalContext(ContextType.Domain, domainName, User, Password);
+
+            Password = "";
+
+            GroupPrincipal grp = GroupPrincipal.FindByIdentity(ctx, IdentityType.SamAccountName, groupName);
+
+            if (grp != null)
+            {
+                foreach (Principal p in grp.GetMembers(false))
+                {
+                    Console.WriteLine(p.SamAccountName + " - " + p.DisplayName);
+                }
 
 
-        //        grp.Dispose();
-        //        ctx.Dispose();
-        //        Console.ReadLine();
-        //    }
-        //    else
-        //    {
-        //        Console.WriteLine("\nWe did not find that group in that domain, perhaps the group resides in a different domain?");
-        //        Console.ReadLine();
-        //    }
-        //}
+                grp.Dispose();
+
+                //Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("\nWe did not find that group in that domain, perhaps the group resides in a different domain?");
+                //Console.ReadLine();
+            }
+
+            // define a "query-by-example" principal - here, we search for a GroupPrincipal 
+            
+        
+            UserPrincipal qbeGroup = new UserPrincipal(ctx);
+
+            // create your principal searcher passing in the QBE principal    
+            PrincipalSearcher srch = new PrincipalSearcher(qbeGroup);
+
+            // find all matches
+            foreach (UserPrincipal found in srch.FindAll())
+            {
+                
+                Console.WriteLine(found.Name);
+
+                foreach (GroupPrincipal g in found.GetGroups(ctx))
+                {
+                    Console.WriteLine("\t{0}", g.Name);
+                }
+
+                // do whatever here - "found" is of type "Principal" - it could be user, group, computer.....          
+            }
+
+            
+        }
+    }
+
+
+
+    public class Example
+    {
+        public static void Test()
+        {
+            // Instantiate the secure string.
+            SecureString securePwd = new SecureString();
+            ConsoleKeyInfo key;
+
+            Console.Write("Enter password: ");
+            do
+            {
+                key = Console.ReadKey(true);
+
+                // Ignore any key out of range. 
+                if (((int)key.Key) >= 32 && ((int)key.Key <= 128))
+                {
+                    // Append the character to the password.
+                    securePwd.AppendChar(key.KeyChar);
+                    Console.Write("*");
+                }
+                // Exit if Enter key is pressed.
+            } while (key.Key != ConsoleKey.Enter);
+            Console.WriteLine();
+
+            try
+            {
+                Process.Start("Notepad.exe", "MyUser", securePwd, "MYDOMAIN");
+            }
+            catch (Win32Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
     }
 }
 
